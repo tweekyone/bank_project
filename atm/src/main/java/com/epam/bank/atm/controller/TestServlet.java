d659a84bffa53d839f0088c5695b9aef535073e3
Dmitrii Korolev <dmitrii_korolev4@epam.com>
2021-09-28 12:12:34 +0300

    [EPMLSTRVEN-4] docker-compose with tomcat service providing debug configuration + test servlet

---
+++ atm/src/main/java/com/epam/bank/atm/controller/TestServlet.java
@@ -0,0 +1,18 @@
+package com.epam.bank.atm.controller;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class TestServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+
+        try (var writer = response.getWriter()) {
+            writer.println("From ATM with love");
+        }
+    }
+}
