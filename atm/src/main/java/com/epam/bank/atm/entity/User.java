5e568959750a67d4256f1c8fb66d1d07d29ef836
Tatiana Naumovets <Tatiana_Naumovets@epam.com>
2021-10-04 20:30:27 +0300

    Create User Repository

---
+++ atm/src/main/java/com/epam/bank/atm/entity/User.java
@@ -0,0 +1,21 @@
+package com.epam.bank.atm.entity;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class User {
+    private Long id;
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+    private String phone_number;
+    private Role role;
+
+    public enum Role {
+        admin,
+        client
+    }
+}
---
+++ atm/src/main/java/com/epam/bank/atm/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.epam.bank.atm.repository;
+
+import com.epam.bank.atm.entity.User;
+
+public interface UserRepository {
+
+    User getById(Long id);
+}
