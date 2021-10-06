ca8cad0a922170425420f08a8fed3ffb7022171a
Dmitrii Korolev <dmitrii_korolev4@epam.com>
2021-10-06 16:21:15 +0300

    [EPMLSTRVEN-16] card entity

--- atm/src/main/java/com/epam/bank/atm/entity/Card.java
+++ atm/src/main/java/com/epam/bank/atm/entity/Card.java
@@ -1,31 +1,60 @@
 package com.epam.bank.atm.entity;
 
+import com.epam.bank.atm.domain.statement.Assertion;
+import com.epam.bank.atm.domain.statement.CardAccountExists;
+import com.epam.bank.atm.domain.statement.CardNumberFormatIsValid;
+import com.epam.bank.atm.domain.statement.CardPinCodeFormatIsValid;
+import com.epam.bank.atm.domain.statement.CardWithSuchNumberDoesNotExist;
+import lombok.Getter;
+import lombok.NonNull;
+import java.time.LocalDateTime;
+
+@Getter
 public class Card {
-    private final long id;
+    private Long id;
     private final String number;
     private final long accountId;
     private final String pinCode;
+    private final Plan plan;
+    private final LocalDateTime expirationDate;
 
-    public Card(long id, String number, long accountId, String pinCode) {
+    public enum Plan {
+        TESTPLAN
+    }
+
+    public Card(
+        @NonNull String number,
+        long accountId,
+        @NonNull String pinCode,
+        @NonNull Plan plan,
+        @NonNull LocalDateTime expirationDate
+    ) {
+        Assertion.assertA(new CardNumberFormatIsValid(number));
+        Assertion.assertA(new CardWithSuchNumberDoesNotExist(number));
+        Assertion.assertA(new CardAccountExists(accountId));
+        Assertion.assertA(new CardPinCodeFormatIsValid(pinCode));
+
+        this.number = number;
+        this.pinCode = pinCode;
+        this.plan = plan;
+        this.expirationDate = expirationDate;
+        this.accountId = accountId;
+    }
+
+    // Hydration constructor
+    public Card(
+        long id,
+        @NonNull String number,
+        long accountId,
+        @NonNull String pinCode,
+        @NonNull Plan plan,
+        @NonNull LocalDateTime expirationDate
+    ) {
         this.id = id;
         this.number = number;
-        this.accountId = accountId;
         this.pinCode = pinCode;
-    }
-
-    public long getId() {
-        return this.id;
-    }
-
-    public String getNumber() {
-        return this.number;
-    }
-
-    public long getAccountId() {
-        return this.accountId;
-    }
-
-    public String getPinCode() {
-        return this.pinCode;
+        this.plan = plan;
+        this.expirationDate = expirationDate;
+        this.accountId = accountId;
     }
 }
