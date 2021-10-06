7d64ea83b8ceca376d2a39f860153aa7a9758a1f
Dmitrii Korolev <dmitrii_korolev4@epam.com>
2021-10-06 16:18:10 +0300

    [EPMLSTRVEN-16] card repository returns optional

--- atm/src/main/java/com/epam/bank/atm/repository/CardRepository.java
+++ atm/src/main/java/com/epam/bank/atm/repository/CardRepository.java
@@ -1,8 +1,10 @@
 package com.epam.bank.atm.repository;
 
 import com.epam.bank.atm.entity.Card;
+import java.util.Optional;
 
 public interface CardRepository {
-    Card getById(long id);
-    Card getByNumber(String number);
+    Optional<Card> getById(long id);
+
+    Optional<Card> getByNumber(String number);
 }
