90190025b5f67219885c522bdf748893d62fe490
Ponyuhov <tweeky92@gmail.com>
2021-10-07 11:37:38 +0300

    Added tests in AuthServiceTest

--- atm/src/test/java/com/epam/bank/atm/service/AuthServiceTest.java
+++ atm/src/test/java/com/epam/bank/atm/service/AuthServiceTest.java
@@ -1,13 +1,20 @@
 package com.epam.bank.atm.service;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.epam.bank.atm.domain.model.AuthDescriptor;
+import com.epam.bank.atm.entity.Account;
 import com.epam.bank.atm.entity.Card;
+import com.epam.bank.atm.entity.User;
 import com.epam.bank.atm.repository.AccountRepository;
 import com.epam.bank.atm.repository.CardRepository;
 import com.epam.bank.atm.repository.UserRepository;
+import java.lang.reflect.Field;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.mockito.Mockito.*;
+import org.mockito.Mockito;
 
 public class AuthServiceTest {
     private AccountRepository mockAccountRepository;
@@ -16,49 +23,116 @@ public class AuthServiceTest {
 
     private AuthService authService;
 
+    @BeforeEach
+    public void init() {
+        authService = new AuthServiceImpl();
+
+        mockAccountRepository = mock(AccountRepository.class);
+        mockCardRepository = mock(CardRepository.class);
+        mockUserRepository = mock(UserRepository.class);
+
+        Class authServiceClass = authService.getClass();
+        try {
+            Field accountRepositoryField = authServiceClass.getDeclaredField("accountRepository");
+            accountRepositoryField.setAccessible(true);
+            accountRepositoryField.set(authService, mockAccountRepository);
+
+            Field cardRepositoryField = authServiceClass.getDeclaredField("cardRepository");
+            cardRepositoryField.setAccessible(true);
+            cardRepositoryField.set(authService, mockCardRepository);
+
+            Field userRepositoryField = authServiceClass.getDeclaredField("userRepository");
+            userRepositoryField.setAccessible(true);
+            userRepositoryField.set(authService, mockUserRepository);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Test
-    public void ifCardNumberIsEmpty(){
+    public void ifCardNumberInLoginIsEmpty() {
         String cardNumber = "";
         String pin = "";
 
         try {
-            new AuthServiceImpl().login(cardNumber, pin);
+            authService.login(cardNumber, pin);
             Assertions.fail("Expected IllegalArgumentException");
-        } catch (IllegalArgumentException thrown){
+        } catch (IllegalArgumentException thrown) {
             Assertions.assertEquals("Error! Card number is empty", thrown.getMessage());
         }
     }
 
     @Test
-    public void ifPinIsEmpty(){
+    public void ifPinInLoginIsEmpty() {
         String cardNumber = "123456";
         String pin = "";
 
         try {
-            new AuthServiceImpl().login(cardNumber, pin);
+            authService.login(cardNumber, pin);
             Assertions.fail("Expected IllegalArgumentException");
-        } catch (IllegalArgumentException thrown){
+        } catch (IllegalArgumentException thrown) {
             Assertions.assertEquals("Error! Pin is empty", thrown.getMessage());
         }
     }
 
     @Test
-    public void ifCardIsEmpty(){
-        CardRepository cardRepository = mock(CardRepository.class);
-        Card card = getTestingCard();
-        when(cardRepository.getById(card.getAccountId())).thenReturn(null);
+    public void ifCardInLoginIsEmpty() {
+        String cardNumber = "123456";
+        String pin = "1234";
+
+        when(mockCardRepository.getById(Mockito.anyLong())).thenReturn(null);
 
         try {
-            new AuthServiceImpl().login("123456", "1234");
+            authService.login(cardNumber, pin);
             Assertions.fail("Expected IllegalArgumentException");
-        } catch (IllegalArgumentException thrown){
+        } catch (IllegalArgumentException thrown) {
             Assertions.assertEquals("Error! Card number is incorrect", thrown.getMessage());
         }
-
-
     }
 
-    public Card getTestingCard(){
-        return new Card(1L, 123456, 54321, 1234);
+    @Test
+    public void ifPinParameterInLoginIsIncorrect() {
+        String cardNumber = "123456";
+        String pin = "4321";
+
+        when(mockCardRepository.getById(Mockito.anyLong())).thenReturn(getTestingCard());
+
+        try {
+            authService.login(cardNumber, pin);
+            Assertions.fail("Expected IllegalArgumentException");
+        } catch (IllegalArgumentException thrown){
+            Assertions.assertEquals("Error! Pin code is incorrect", thrown.getMessage());
+        }
     }
+
+    @Test
+    public void ifParametersInLoginIsCorrect() {
+        String cardNumber = "123456";
+        String pin = "1234";
+
+        when(mockCardRepository.getById(Mockito.anyLong())).thenReturn(getTestingCard());
+        when(mockAccountRepository.getById(Mockito.anyLong())).thenReturn(getTestingAccount());
+        when(mockUserRepository.getById(Mockito.anyLong())).thenReturn(getTestingUser());
+
+        AuthDescriptor testAuthDescriptor = authService.login(cardNumber, pin);
+        AuthDescriptor controlAuthDescriptor =
+            new AuthDescriptor(getTestingUser(), getTestingAccount(), getTestingCard());
+
+        Assertions.assertEquals(controlAuthDescriptor, testAuthDescriptor);
+    }
+
+    public Card getTestingCard() {
+        return new Card(123456, 123456, 54321, 1234);
+    }
+
+    public Account getTestingAccount() {
+        return new Account(54321, 1324);
+    }
+
+    public User getTestingUser() {
+        return new User(1324, "Name",
+            "Surname", "8(911)123-12-32",
+            "Username", "email@mail.com", "password");
+    }
+
 }
