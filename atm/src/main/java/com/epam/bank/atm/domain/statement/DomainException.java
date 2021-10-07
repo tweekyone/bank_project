97b0feafc2eb612b47f8b25b46ce293a08ec4530
Dmitrii Korolev <dmitrii_korolev4@epam.com>
2021-10-07 12:32:18 +0300

    [EPMLSTRVEN-12] Statement as a property of DomainException

--- atm/src/main/java/com/epam/bank/atm/domain/statement/DomainException.java
+++ atm/src/main/java/com/epam/bank/atm/domain/statement/DomainException.java
@@ -1,7 +1,14 @@
 package com.epam.bank.atm.domain.statement;
 
 public class DomainException extends RuntimeException {
+    private final Statement statement;
+
     public DomainException(Statement statement) {
         super(statement.errorMessage());
+        this.statement = statement;
+    }
+
+    public Statement getStatement() {
+        return this.statement;
     }
 }
