02cf4d87ab07776d2857f82632d65bf2a4554d58
Evgenii Ponukhov <evgenii_ponukhov@epam.com>
2021-10-08 18:07:59 +0300

    Corrected DIContainer.java and UserRepository.java

--- atm/src/main/java/com/epam/bank/atm/repository/UserRepository.java
+++ atm/src/main/java/com/epam/bank/atm/repository/UserRepository.java
@@ -1,10 +1,7 @@
 package com.epam.bank.atm.repository;
 
 import com.epam.bank.atm.entity.User;
-import java.util.List;
 
 public interface UserRepository {
     User getById(long id);
-    void save(User user);
-    List<User> getAll();
 }
