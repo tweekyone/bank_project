ee3a3304e4da55c9caf2ee342a12753581fc7061
Dmitrii Korolev <dmitrii_korolev4@epam.com>
2021-10-11 19:43:42 +0300

    [EPMLSTRVEN-16] fixes di factory methods

--- atm/src/main/java/com/epam/bank/atm/di/DIContainer.java
+++ atm/src/main/java/com/epam/bank/atm/di/DIContainer.java
@@ -145,10 +145,15 @@ public class DIContainer {
     }
 
     private TransactionalService createTransactionalService() {
-        return new TransactionService(this.getSingleton(TransactionRepository.class));
+        return new TransactionService(
+            this.getSingleton(TransactionRepository.class, this::createTransactionRepository)
+        );
     }
 
-    private AccountService createAccountService(){
-        return new AccountService(createTransactionalService(), this.getSingleton(AccountRepository.class));
+    private AccountService createAccountService() {
+        return new AccountService(
+            this.getSingleton(TransactionalService.class, this::createTransactionalService),
+            this.getSingleton(AccountRepository.class, this::createAccountRepository)
+        );
     }
 }
