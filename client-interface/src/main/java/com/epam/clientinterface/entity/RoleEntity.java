334d95f2e5ffcba6d330bf22f7b367ce315f88ff
Nikita Golubev <nikita_golubev@epam.com>
2021-10-20 13:36:03 +0300

    init client interface module

--- client-interface/src/main/java/com/epam/clientinterface/entity/RoleEntity.java
+++ client-interface/src/main/java/com/epam/clientinterface/entity/RoleEntity.java
@@ -21,7 +21,7 @@ import lombok.Setter;
 public class RoleEntity {
 
     @Id
-    @SequenceGenerator(name = "role_id_seq", sequenceName = "role_id_seq")
+    @SequenceGenerator(name = "role_id_seq", sequenceName = "role_id_seq", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "role_id_seq")
     private Long id;
 
