3929089ace82a72fd6d422b93fe2a13f2c990ff0
Dmitrii Korolev <dmitrii_korolev4@epam.com>
2021-10-24 18:37:13 +0300

    [EPMLSTRVEN-24] modifies type of status in error response

--- client-interface/src/main/java/com/epam/clientinterface/controller/dto/response/ErrorResponse.java
+++ client-interface/src/main/java/com/epam/clientinterface/controller/dto/response/ErrorResponse.java
@@ -1,14 +1,25 @@
 package com.epam.clientinterface.controller.dto.response;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NonNull;
 import org.springframework.http.HttpStatus;
 
 @Getter
-@AllArgsConstructor
 public class ErrorResponse {
     private final String type;
-    private final HttpStatus status;
+    private final short status;
     private final String title;
     private final String detail;
+
+    public ErrorResponse(
+        @NonNull String type,
+        @NonNull HttpStatus status,
+        @NonNull String title,
+        @NonNull String detail
+    ) {
+        this.type = type;
+        this.status = (short) status.value();
+        this.title = title;
+        this.detail = detail;
+    }
 }
