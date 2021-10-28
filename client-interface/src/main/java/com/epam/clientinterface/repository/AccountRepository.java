9cc9fc2d579e23a8619e61d0d41956c6ab17c53e
Dmitrii Korolev <dmitrii_korolev4@epam.com>
2021-10-28 16:13:27 +0300

    [EPMLSTRVEN-25] adds findByNumber query

--- client-interface/src/main/java/com/epam/clientinterface/repository/AccountRepository.java
+++ client-interface/src/main/java/com/epam/clientinterface/repository/AccountRepository.java
@@ -1,7 +1,9 @@
 package com.epam.clientinterface.repository;
 
 import com.epam.clientinterface.entity.Account;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
+    Optional<Account> findByNumber(String number);
 }
