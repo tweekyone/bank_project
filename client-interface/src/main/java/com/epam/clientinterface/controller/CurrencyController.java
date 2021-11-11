04e30ded1ff6165353afec3fef0e39f52a38884c
RenatFaiz <renatf87@gmail.com>
2021-11-11 22:35:12 +0300

    Currency controller returns exchange rates: from and from/to currency from ExchangeRateRepository

--- client-interface/src/main/java/com/epam/clientinterface/controller/CurrencyController.java
+++ client-interface/src/main/java/com/epam/clientinterface/controller/CurrencyController.java
@@ -1,5 +1,6 @@
 package com.epam.clientinterface.controller;
 
+import com.epam.clientinterface.entity.Currency;
 import com.epam.clientinterface.entity.ExchangeRate;
 import com.epam.clientinterface.repository.ExchangeRateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,20 +8,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/exchange-rates")
 public class CurrencyController {
 
-    // @Autowired
-    // private CurrencyRepository currencyRepository;
-
     @Autowired
     private ExchangeRateRepository rateRepository;
-
-    // private Map<String, Double> currencyRates = new HashMap<>();
 
     @GetMapping("/get")
     public String testCurrencyRates() {
@@ -28,20 +28,21 @@ public class CurrencyController {
     }
 
     @GetMapping("/{from}")
-    public ResponseEntity<ExchangeRate> getCurrencyRates(@PathVariable("from") String currency) {
+    public ResponseEntity<List<ExchangeRate>> getCurrencyRates(@PathVariable("from") Currency currency) {
 
-        ExchangeRate exchangeRate1 = rateRepository.getAllById((short) 1);
+        // ExchangeRate exchangeRate1 = rateRepository.getAllById((short) 1);
+        List<ExchangeRate> exchangeRates = rateRepository.getExchangeRatesByCurrencyFrom(currency);
 
-        return new ResponseEntity<>(exchangeRate1, HttpStatus.OK);
+        return new ResponseEntity<>(exchangeRates, HttpStatus.OK);
     }
 
     @GetMapping("{from}/{to}")
     public ResponseEntity<ExchangeRate> getExchangeRates(
-        @PathVariable("from") String currencyFrom,
-        @PathVariable("to") String currencyTo
+         @PathVariable("from") Currency currencyFrom,
+         @NotNull @PathVariable("to") Currency currencyTo
     ) {
+        Optional<ExchangeRate> oneByCurrencies = rateRepository.findOneByCurrencies(currencyFrom, currencyTo);
 
-
-        return new ResponseEntity<>(null, HttpStatus.OK);
+        return new ResponseEntity<>(oneByCurrencies.get(), HttpStatus.OK);
     }
 }
