75f4c7162d1d0dddf729d8323e35561adfcd6add
Dmitrii Korolev <dmitrii_korolev4@epam.com>
2021-11-22 14:47:47 +0300

    [EPMLSTRVEN-37] adds FixtureFactory which incapsulates creation entities of different scenarios

---
+++ client-interface/src/test/java/com/epam/clientinterface/FixtureFactory.java
@@ -0,0 +1,62 @@
+package com.epam.clientinterface;
+
+import com.epam.clientinterface.entity.Account;
+import com.epam.clientinterface.entity.Card;
+import com.epam.clientinterface.entity.CardPlan;
+import com.epam.clientinterface.entity.User;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.RandomUtils;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class FixtureFactory {
+    public static Account getAccount() {
+        return new Account(
+            RandomUtils.nextLong(),
+            RandomStringUtils.randomNumeric(20),
+            RandomUtils.nextBoolean(),
+            Account.Plan.values()[RandomUtils.nextInt(0, Account.Plan.values().length)],
+            RandomUtils.nextDouble(10000.0, 100000.0),
+            new User(),
+            new ArrayList<>(),
+            null
+        );
+    }
+
+    public static Account getClosedAccount() {
+        return new Account(
+            RandomUtils.nextLong(),
+            RandomStringUtils.randomNumeric(20),
+            RandomUtils.nextBoolean(),
+            Account.Plan.values()[RandomUtils.nextInt(0, Account.Plan.values().length)],
+            RandomUtils.nextDouble(10000.0, 100000.0),
+            new User(),
+            new ArrayList<>(),
+            LocalDateTime.now()
+        );
+    }
+
+    public static Card getCard() {
+        return new Card(
+            RandomUtils.nextLong(),
+            RandomStringUtils.randomNumeric(16),
+            getAccount(),
+            RandomStringUtils.randomNumeric(4),
+            CardPlan.values()[RandomUtils.nextInt(0, CardPlan.values().length)],
+            false,
+            LocalDateTime.now().plusYears(3)
+        );
+    }
+
+    public static Card getCardWithClosedAccount() {
+        return new Card(
+            RandomUtils.nextLong(),
+            RandomStringUtils.randomNumeric(16),
+            getClosedAccount(),
+            RandomStringUtils.randomNumeric(4),
+            CardPlan.values()[RandomUtils.nextInt(0, CardPlan.values().length)],
+            false,
+            LocalDateTime.now().plusYears(3)
+        );
+    }
+}
